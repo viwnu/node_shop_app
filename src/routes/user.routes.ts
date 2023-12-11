b85826d782e06a919fd646237c47d45699f1a3ec
viwnu <xenmysafitiplace@gmail.com>
2023-12-11 21:35:57 +0300

    fix user.routes

--- src/routes/user.routes.ts
+++ src/routes/user.routes.ts
@@ -1,17 +1,24 @@
 import express, { Request, Response } from 'express'
 
-import userController from '../db.services/single.table.db.service'
 import userCartRouter from './user.cart.routes'
 import userOrdersRouter from './user.orders.routes'
+import { AppDataSource } from '../data-source'
+import { Users } from '../entity/Users'
+import { ApiError } from '../exceptions/api.error'
 
 const router = express.Router()
 
 router.use('/cart', userCartRouter)
 router.use('/orders', userOrdersRouter)
 
-router.get('/', async (req: Request, res: Response) => {
-    const response = await userController.getByField('users', 'user_id', req.body.user_id)
-    res.json(response instanceof TypeError ?response :response[0])
+router.get('/:id', async (req: Request, res: Response, next: (arg0: ApiError) => void) => {
+
+    try {
+        const users = await AppDataSource.manager.findOneBy(Users, {user_id: Number(req.params.id)})
+        res.json(users)
+    } catch (error) {
+        next(error as ApiError)
+    }
 })
 
 export default router
